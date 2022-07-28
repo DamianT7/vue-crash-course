4bcd8f13a9d6289cfc45b1bf30cd666cd9212a24
Damian Teske <damiant7@live.com>
2022-07-28 11:07:07 +0200

    section 4

---
+++ section4/src/components/Posts.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <p>Wassup Damian</p>
+    <button class="btn btn-secondary">Click</button>
+
+
+    <table class="table table-striped mt-4">
+      <thead>
+        <tr>
+          <th scope="col">UserId</th>
+          <th scope="col">Id</th>
+          <th scope="col">Title</th>
+          <th scope="col">Body</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="post in posts" :key="post.id">
+          <td>{{post.userId}}</td>
+          <td>{{post.id}}</td>
+          <td>{{post.title}}</td>
+          <td>{{post.body}}</td>
+        </tr>
+      </tbody>
+    </table>
+
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default{
+    name: 'PostsExample',
+    data(){
+      return {
+        // posts: [
+        //   {id:1,title:'Damian Post 1',body:'ngkusnfkdsnkfdns'},
+        //   {id:2,title:'Damian Post 2',body:'ngkusnfkdsnkfdns'},
+        //   {id:3,title:'Damian Post 3',body:'ngkusnfkdsnkfdns'},
+        //   {id:4,title:'Damian Post 4',body:'ngkusnfkdsnkfdns'},
+        //   {id:5,title:'Damian Post 5',body:'ngkusnfkdsnkfdns'},
+        // ],
+        posts: []
+      }
+    },
+    created(){
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(posts =>{
+          this.posts = posts.data
+        })
+    }
+  }
+
+</script>
+
+<style>
+
+</style>
---
+++ section4/src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+// import VueRouter from 'vue-router'
+import Posts from './components/Posts'
+
+// Vue.use(VueRouter);
+const routes = [
+  {
+    path: '/', component: Posts
+  }
+
+
+
+]
+
+Vue.config.productionTip = false;
+
+
+// const router = new VueRouter({routes: routes});
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  // router,
+  components: { App },
+  template: '<App/>'
+})
